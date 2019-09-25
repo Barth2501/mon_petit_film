57314a091a89c00e8a3b1a314282d4e88800461e
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2019-09-25 20:01:30 +0200

    Add mongodb, create a dao class

--- manage.py
+++ manage.py
@@ -2,17 +2,21 @@ from server import app
 import os
 import sys
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 available_commands = ('runserver',)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2 or sys.argv[1] != 'runserver':
+    if len(sys.argv) != 2 or sys.argv[1] not in available_commands:
         print('\nWrong command, please use a rightful command following this',
               ' typo:\tpython manage.py <COMMAND>\n\nList of available',
               'commands :\n' + '\n'.join(available_commands))
     else:
         load_dotenv()
+        # Launch db
+        client = MongoClient(os.environ.get('MONGODB_URI'))
+        db = client.get_default_database()
+        # Launch app
         debug = os.environ.get('FLASK_ENV', 'production') == 'development'
-        # Bind to PORT if defined, otherwise default to 5000.
         port = int(os.environ.get('PORT', 5000))
         app.run(host='localhost', port=port, debug=debug)
