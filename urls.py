603bda8d94767894854209a08e62a78f178003a7
barthelemy <barthelemy.lancelot@zenrooms.com>
2019-09-25 17:38:44 +0200

    index.html

--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index(name=None):
     return render_template('index.html', name=name)
+
+@app.route('/movies')
+def movies():
+    return render_template('movies.html', context={})
