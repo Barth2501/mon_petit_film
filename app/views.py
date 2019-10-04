ef41c526a8c333a140853e812ddf54201d4540d7
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2019-10-04 09:56:20 +0200

    Fix views.py file
    Code style, conflicts, print unwanted

--- app/views.py
+++ app/views.py
@@ -1,17 +1,11 @@
-import os
-from flask import render_template, redirect, url_for,Flask
+# import os
+from flask import render_template, redirect, url_for, Flask, request, jsonify
 from flask_pymongo import PyMongo
-<<<<<<< HEAD
-from flask import request
-from flask import jsonify
-import pandas as pd
-=======
->>>>>>> d1e88ab098444bb22f992416e69d788233da1a80
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'restdb'
-#app.config['MONGO_URI'] = os.environ.get('MONGODB_URI')
+# app.config['MONGO_URI'] = os.environ.get('MONGODB_URI')
 app.config['MONGO_URI'] = 'mongodb://heroku_1hj3v1h2:hiiq0l9nuj1fdffsqffr6spc1p@ds113799.mlab.com:13799/heroku_1hj3v1h2?retryWrites=false'
 
 mongo = PyMongo(app)
@@ -30,42 +24,38 @@ def index(name=None):
 @app.route('/movies')
 def movies():
     genres = mongo.db.genres
-<<<<<<< HEAD
-=======
-    # genres = pd.DataFrame(list(genres.find()))
-    # for genre in genres.find():
-    #     print(genre['name'])
->>>>>>> d1e88ab098444bb22f992416e69d788233da1a80
     return render_template('movies.html', genres=genres)
+
 
 @app.route('/movies/genre=<int:genre_id>')
 def genre(genre_id):
     movies = mongo.db.movies
-    return render_template('genre.html', movies = movies, genre_id=genre_id)
+    return render_template('genre.html', movies=movies, genre_id=genre_id)
 
-@app.route('/movie', methods = ['POST'])
+
+@app.route('/movie', methods=['POST'])
 def add_movie():
     movie = mongo.db.movies
-    try :
+    try:
         name = request.json['name']
         description = request.json['description']
-        movie_id = movie.insert({'name':name,'description':description})
-        new_movie = movie.find_one({'_id': movie_id })
-        output = {'name' : new_movie['name']}
-        return jsonify({'result' : output})
+        movie_id = movie.insert({'name': name, 'description': description})
+        new_movie = movie.find_one({'_id': movie_id})
+        output = {'name': new_movie['name']}
+        return jsonify({'result': output})
     except TypeError:
-        return jsonify({'result' : 'niet'})
+        return jsonify({'result': 'niet'})
 
-@app.route('/movie', methods = ['GET'])
+
+@app.route('/movie', methods=['GET'])
 def all_movie():
     movie = mongo.db.movies
     output = []
     for s in movie.find():
-        print(s)
         try:
             output.append({
-            'original_title':s['original_title']
-        })
+                'original_title': s['original_title']
+            })
         except KeyError:
             continue
-    return jsonify({'result' : output})
+    return jsonify({'result': output})
