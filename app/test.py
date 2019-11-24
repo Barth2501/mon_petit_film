66ab6c36b2a68c27fbdfb0ac1d5f91d0fd082796
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2019-11-24 22:11:55 +0100

    remove useless script file

--- app/test.py
+++
@@ -1,8 +0,0 @@
-from classes.movies_and_series import TVShow
-
-def tvshow():
-    # if 'username' not in session or 'id' not in session:
-    #     return redirect(url_for('index'))
-    tvshow = TVShow.filter_json()
-    print(tvshow)
-tvshow()
