4e2dc96bf856818c2a454c51c197d406031fc825
Barth2501 <barthelemy.lancelot@student.ecp.fr>
2019-11-25 19:57:14 +0100

    change port

--- manage.py
+++ manage.py
@@ -19,8 +19,4 @@ if __name__ == "__main__":
         debug = os.environ.get("FLASK_ENV", "production") == "development"
         port = int(os.environ.get("PORT", 5000))
         app.secret_key = os.urandom(12)
-<<<<<<< HEAD
         app.run(host='0.0.0.0', port=port, debug=debug)
-=======
-        app.run(host="localhost", port=port, debug=debug)
->>>>>>> 9e8f079e91c2c2f4f4d68e0d59675f858a7e9d55
