a030d565a18783fcc0c6915efb976885947cc485
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2019-11-25 22:07:11 +0100

    Resolve config conflicts

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 from celery.schedules import crontab
 
-<<<<<<< HEAD
 CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_IMPORTS = ('cronjobs.tasks.test', 'cronjobs.tasks.send_mail')
@@ -8,15 +7,6 @@ CELERYBEAT_SCHEDULE = {
     'send-mail-on-monday': {
         'task': 'cronjobs.tasks.send_mail.send_mail_flask',
         'schedule': crontab(minute="*"),
-=======
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
-CELERY_IMPORTS = ("app.tasks.test", "app.tasks.send_mail")
-CELERYBEAT_SCHEDULE = {
-    "send-mail-on-monday": {
-        "task": "app.tasks.send_mail.send_mail_flask",
-        "schedule": crontab(minute="*"),
->>>>>>> 9e8f079e91c2c2f4f4d68e0d59675f858a7e9d55
     }
 }
 MAIL_SERVER = "smtp.gmail.com"
